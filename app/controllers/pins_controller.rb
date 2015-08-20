e970de227c43a5bb89cff3d64bfc1894eeae5f7c
choonggg <iamchoonggg@gmail.com>
2015-08-20 16:26:36 +0800

    votable, views, logics, done

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,6 @@
 class PinsController < ApplicationController
-  before_action :find_pin, only: [:show, :edit, :update, :destroy]
+  before_action :find_pin, only: [:show, :edit, :update, :destroy, :upvote]
+  before_action :authenticate_user!, except: [:show, :index]
   def index
     @pins = Pin.all.order("created_at DESC")
   end
@@ -33,6 +34,11 @@ class PinsController < ApplicationController
   def destroy
     @pin.destroy
     redirect_to root_path
+  end
+
+  def upvote
+    @pin.upvote_by current_user
+    redirect_to :back
   end
 
   private
